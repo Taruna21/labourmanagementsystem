93486ba232074bf83ad62894d56a36d254339c3f
Taruna21 <pateltaruna552@gmail.com>
2026-02-05 22:08:42 +0530

    first commit

---
+++ accounts/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.contrib.auth.models import User
+from . models import Profile
+
+
+
+class signupForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    role = forms.ChoiceField(choices=Profile.ROLE_CHOICES)
+    city = forms.CharField(max_length=100)
+    role = forms.CharField(max_length=100)
+    area = forms.CharField(max_length=100)
+
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+            profile = user.Profile
+            profile.role = self.cleaned_data['role']
+            profile.city = self.cleaned_data['city']
+            profile.area = self.cleaned_data['area']
+            profile.save()
+        return user
+        
+        
---
+++ accounts/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name    
+    
+class Profile(models.Model):
+    ROLE_CHOICES = (
+        ('provider', 'Service Provider'),
+        ('seeker', 'Service Seeker'),
+    )
+    User = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    Category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    city = models.CharField(max_length=100)
+    area = models.CharField(max_length=100)
+    is_available = models.BooleanField(max_length=100)
+
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
